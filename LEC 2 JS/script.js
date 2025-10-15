ceb90e9d2d8b1516721b4dced5eadcdd2447e5fc
hansrajathole <hansrajathole1@gmail.com>
2025-10-15 09:08:54 +0530

    Add HTML structure and initial JavaScript code with variable declarations

---
+++ LEC 2 JS/script.js
@@ -0,0 +1,73 @@
+
+
+// let promise = new Promise((resolve , reject)=>{
+//     setTimeout(()=>{
+//         let num = Math.floor(Math.random()*2)
+//         console.log(num);
+        
+//         if(num === 1){
+//             resolve("bday ki party krte hai")
+//         }else {
+//             reject("koi party nhi milegi")
+//         }
+//     },3000)
+// })
+
+
+// console.log(promise);
+// promise
+// .then((res)=>{
+//     console.log(res);
+//     console.log(promise);
+    
+// })
+// .catch((err)=>{
+//     console.log(err);
+//     console.log(promise);
+    
+// })
+
+
+
+// async function getData() {
+//   try {
+//     const response = await fetch("https://dummyjson.com/users")
+//     let result = await response.json()
+
+//     console.log(result);
+    
+
+//   } catch (error) {
+//     console.log(error);
+    
+//   }
+  
+// }
+
+// getData()
+
+
+
+// localStorage.setItem("name","hansraj")
+// localStorage.setItem("age","23")
+// localStorage.setItem("state","MH")
+
+// // localStorage.removeItem("state")
+// // // localStorage.clear()
+
+// console.log(localStorage.getItem("name"));
+
+
+// {
+//     name : "hansraj"
+// }
+
+
+console.log(b);
+var b = 10
+console.log(a);
+let a = 20
+// console.log(c);
+console.log(a);
+
+
