840209e8f1a1802442b7a09e918a1e85a5433de2
hansrajathole <hansrajathole1@gmail.com>
2025-10-27 20:50:17 +0530

    Add initial HTML, CSS, and JavaScript files with basic structure and functionality

---
+++ LEC 4 DOM/script.js
@@ -0,0 +1,30 @@
+const parent = document.getElementsByClassName("parent")
+// const child1 = document.getElementById("child1")
+// const para = document.getElementsByTagName("p")
+const para = document.querySelector("p")
+// const paraAll = document.querySelectorAll("p")
+
+// console.log(parent);
+// console.log(child1);
+// console.log(para);
+// console.log(para);
+// console.log(paraAll);
+
+
+// para.innerText = "Hansraj <b >Hansraj</b>"
+// para.textContent = "Dev <b >Hansraj</b>"
+
+// const name = "Raj"
+// para.innerHTML = `hello <b >${name}</b>`
+
+
+// console.log(para.innerText);
+// console.log(para.textContent);
+// console.log(para.innerHTML);
+
+parent[0].style.backgroundColor = "red"
+
+
+
+
+
