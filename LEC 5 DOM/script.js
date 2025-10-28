aa511d9d4a6bb85486227d77c730a49bc2a22fd5
hansrajathole <hansrajathole1@gmail.com>
2025-10-28 21:21:04 +0530

    dom done

---
+++ LEC 5 DOM/script.js
@@ -0,0 +1,52 @@
+// const div = document.querySelector("div")
+
+// // div.classList.add("box")
+
+// // div.classList.remove("box")
+
+
+// const btn = document.querySelector("button")
+// const body = document.querySelector("body")
+
+// let istrue = false
+
+// btn.addEventListener("click",()=>{
+//    if(istrue){
+//      btn.innerText = "Dark"
+//      istrue = false
+//      console.log(body.classList.contains("dark"));
+     
+//    }else {
+//      btn.innerText = "light"
+//      istrue = true
+//      console.log(body.classList.contains("dark"));
+
+//    }
+
+//    body.classList.toggle("dark")
+// })
+
+
+
+
+
+const btn = document.querySelector("button")
+const body = document.querySelector("body")
+
+let imgSrc = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRXIonIMvIShPTxQdYUxA-QF8wl825JWYaiTQ&s"
+
+
+btn.addEventListener("click", ()=>{
+   let img = document.createElement("img")
+   img.setAttribute("src",imgSrc) 
+
+   img.style.position = "absolute"
+   let top = Math.floor(Math.random()*90)
+   let left = Math.floor(Math.random()*90)
+
+   img.style.top = top+"%"
+   img.style.left = left+"%"
+
+   body.appendChild(img)
+
+})
